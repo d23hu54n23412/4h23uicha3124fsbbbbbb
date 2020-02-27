bad82e025d69c1f36f94c15c5f170f1ba8abe116
ToggledReach <PracticalX@gmail.com>
2020-02-26 20:25:58 -0500

    Implemented numerous changes

--- initialize.lua
+++ initialize.lua
@@ -4,7 +4,10 @@
     @author NodeSupport
 --]]
 
-local environment = {}
+local environment = {
+    modules = {}
+}
+
 if getgenv().gg then
     return warn("Battlegrounds Zero Environment is already loaded")
 end
@@ -16,8 +19,11 @@ function environment.load(path)
         return warn("Invalid Pathway for loading module")
     end
     if type(path) == "string" then
-        return loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/ToggledReach/BattlegroundsZero/master/".. path ".lua?token=AKWUNB6C4AOLLCM77SXQ2LK6K37LA"))()
+        return loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/ToggledReach/BattlegroundsZero/master/".. path ".lua"))()
     elseif type(path) == "number" then
         return game:GetObjects("rbxassetid://" .. path)[1]
     end
 end
+
+gg.ui = environment.load(4735247703)
+gg.modules.login = environment.load("Modules/login")
---
+++ loader.lua
@@ -0,0 +1,11 @@
+--[[
+
+    Welcome to Battlegrounds Zero,
+    Version 1.0
+
+    Report all bugs to the discord server, make sure to authorize yourself
+    with the bot using !whitelist
+
+]]
+
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/ToggledReach/BattlegroundsZero/master/initialize.lua"))()
