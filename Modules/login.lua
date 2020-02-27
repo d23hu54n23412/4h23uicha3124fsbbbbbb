368fe01b65707e474fe8ae9d86cae7d3d86be255
ToggledReach <PracticalX@gmail.com>
2020-02-26 20:47:55 -0500

    Fixed capitalization error

--- Modules/login.lua
+++ Modules/login.lua
@@ -18,52 +18,52 @@ local SETTINGS = {
 }
 
 ui.Parent = game:GetService("CoreGui")
-Loading.Size = UDim2.new(0, 0, 0, 0)
+loading.Size = UDim2.new(0, 0, 0, 0)
 
 wait(SETTINGS.BEGINNING_DELAY)
 
-Loading:TweenSize(UDim2.new(0, 263, 0, 263), "Out", "Back", 2, true)
+loading:TweenSize(UDim2.new(0, 263, 0, 263), "Out", "Back", 2, true)
 
 wait(SETTINGS.LOADING_DELAY)
 
-Loading.Goat:TweenSize(UDim2.new(0, 0, 0, 0), "In", "Back", 0.3, true)
+loading.Goat:TweenSize(UDim2.new(0, 0, 0, 0), "In", "Back", 0.3, true)
 
-Loading:TweenSize(UDim2.new(0, 321, 0, 373), "In", "Linear", 0.4, true)
+loading:TweenSize(UDim2.new(0, 321, 0, 373), "In", "Linear", 0.4, true)
 
-for i = Loading.Circle.SliceScale, 0.4, -.1 do
+for i = loading.Circle.SliceScale, 0.4, -.1 do
 	wait()
-	Loading.Circle.SliceScale = i
+	loading.Circle.SliceScale = i
 end
 
-Loading.Header.Visible = true
-Loading.Header.TextTransparency = 1
+loading.Header.Visible = true
+loading.Header.TextTransparency = 1
 
 spawn(function()
-	for i = Loading.Header.TextTransparency, 0, -.1 do
+	for i = loading.Header.TextTransparency, 0, -.1 do
 		wait()
-		Loading.Header.TextTransparency = i
+		loading.Header.TextTransparency = i
 	end
 end)
 
-Loading.TokenInputBox.Size = UDim2.new(0, 0, 0.107, 0)
-Loading.TokenInputBox.Visible = true
+loading.TokenInputBox.Size = UDim2.new(0, 0, 0.107, 0)
+loading.TokenInputBox.Visible = true
 
-Loading.TokenInputBox:TweenSize(UDim2.new(.821, 0, .107, 0), "In", "Linear", 0.2, true)
+loading.TokenInputBox:TweenSize(UDim2.new(.821, 0, .107, 0), "In", "Linear", 0.2, true)
 
 wait(.2)
 
-Loading.LockIcon.Size = UDim2.new(0, 0, 0, 0)
-Loading.LockIcon.Visible = true
+loading.LockIcon.Size = UDim2.new(0, 0, 0, 0)
+loading.LockIcon.Visible = true
 
 wait()
-Loading.LockIcon:TweenSize(UDim2.new(.077, 0, .062, 0), "Out", "Back", 0.2, true)
+loading.LockIcon:TweenSize(UDim2.new(.077, 0, .062, 0), "Out", "Back", 0.2, true)
 
-Loading.Input.Visible = true
-Loading.Input.TextTransparency = 1
+loading.Input.Visible = true
+loading.Input.TextTransparency = 1
 
 spawn(function()
-	for i = Loading.Input.TextTransparency, 0, -.1 do
+	for i = loading.Input.TextTransparency, 0, -.1 do
 		wait()
-		Loading.Input.TextTransparency = i
+		loading.Input.TextTransparency = i
 	end
 end)
