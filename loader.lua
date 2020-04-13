850fa15cdda6570cba4abc34b5c942e36b0effd9
Node <PracticalX@gmail.com>
2020-04-13 13:21:04 -0400

    Implemented proper warning for when Adonis is bypassed

--- loader.lua
+++ loader.lua
@@ -1,7 +1,7 @@
 --[[
 
     Welcome to Battlegrounds Zero,
-    Version 1.1.2
+    Version 1.1.3
 
     Report all bugs to the discord server, make sure to authorize yourself
     with the bot using !whitelist
