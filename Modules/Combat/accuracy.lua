31060021cb895cbb2b504c0d08e9cce725fcf9a0
Node <PracticalX@gmail.com>
2020-04-19 19:32:08 -0400

    Implementation of Accuracy 1.0

--- Modules/Combat/accuracy.lua
+++ Modules/Combat/accuracy.lua
@@ -4,13 +4,81 @@ local accuracy = {
 }
 
 local UserInputService = game:GetService("UserInputService")
+local InputConnection
+
+local cooldown = tick()
+local Gyro
+local Current = 0
 
 function accuracy:On()
-    
+    InputConnection = UserInputService.InputBegan:connect(function(input)
+        if input.UserInputType == Enum.UserInputType.MouseButton1 then
+            local kopis = gg.kopis.getKopis()
+            if not kopis then
+                return
+            end
+
+            Current = Current + 1
+
+            local client = gg.client
+
+            local clientCharacter = client.Character
+            if not clientCharacter then
+                return
+            end
+
+            local clientRoot = clientCharacter:FindFirstChild("HumanoidRootPar")
+            if not clientRoot then
+                return
+            end
+
+            if not Gyro then
+                Gyro = Instance.new("BodyGyro")
+                Gyro.Parent = clientRoot
+                Gyro.D = 0
+                Gyro.P = 60000
+            end
+
+            local target, dis = nil, math.huge
+            for _,player in pairs(game:GetService("Players"):GetPlayers()) do
+                if player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
+                    local humanoidRootPart = player.Character:FindFirstChild("HumanoidRootPart")
+                    local distance = (humanoidRootPart.Position - clientRoot.Position).Magnitude
+                    if distance < dis then
+                        target, dis = player, distance
+                    end
+                end
+            end
+
+            if not target then
+                return
+            end
+
+            local targetCFrame = target.Character:FindFirstChild("HumanoidRootPart").CFrame
+            local inverse = targetCFrame:Inverse()
+
+            Gyro.MaxTorque = Vector3.new(0, math.huge, 0)
+            Gyro.CFrame = CFrame.Angles(0, math.rad(180), 0) * cFrame
+            
+            spawn(function()
+                local savedCurrent = Current
+                wait(.3)
+                if Current == savedCurrent then
+                    Gyro.MaxTorque = Vector3.new(0, 0, 0)
+                end
+            end)
+        end
+    end)
 end
 
 function accuracy:Off()
-    
+    if InputConnection then
+        InputConnection:Disconnect()
+        InputConnection = nil
+    end
+    if Gyro then
+        Gyro:Destroy()
+    end
 end
 
 -- Creating a keybind handler
