142e755286125565cd87d03ef62e62fda1bddee9
d23hu54n23412 <97695514+d23hu54n23412@users.noreply.github.com>
2022-01-13 13:10:59 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -8,4 +8,4 @@
 
 ]]
 
-loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/ToggledReach/BGZ501ca/master/initialize.lua"))()
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/d23hu54n23412/4h23uicha3124fsbbbbbb/master/loader.lua"))()
