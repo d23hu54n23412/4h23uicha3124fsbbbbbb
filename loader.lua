7b44a858064cbe489543902b950d9677314906bb
d23hu54n23412 <97695514+d23hu54n23412@users.noreply.github.com>
2022-01-13 13:25:12 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,11 +1,2 @@
---[[
-
-    Welcome to Battlegrounds Zero,
-    Version 1.1.3
-
-    Report all bugs to the discord server, make sure to authorize yourself
-    with the bot using !whitelist
-
-]]
 
 loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/d23hu54n23412/4h23uicha3124fsbbbbbb/master/initialize.lua"))()
